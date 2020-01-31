629ae12ee43bb13b8ed29f6b7f7f9b1b14bfb415
elfez <dswegen@gmail.com>
2020-01-31 16:48:51 +0000

    Added keyword filters and inclusion of multiple countries in results

--- BGP Table Fixer.user.js
+++ BGP Table Fixer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BGP Price Cut Table Fixer
 // @namespace    http://crashpr.one
-// @version      0.2
+// @version      0.3
 // @description  Sorts price cut list by date and removes all non-GB entries (see line 34 to mod)
 // @author       You
 // @match        https://boardgameprices.co.uk/item/pricedrops
@@ -12,36 +12,66 @@
 (function() {
     'use strict';
 
-    function sortSpans(a, b) {
-        var aText = a.parentNode.getElementsByTagName("div")[0].textContent.valueOf();
-        var bText = b.parentNode.getElementsByTagName("div")[0].textContent.valueOf();
-        console.log(aText, bText);
-        if (aText > bText) return -1;
-        if (aText < bText) return 1;
-        return 0;
+    // Config
+
+    // List of strings (quoted, in lowercase) to filter out from results
+    // Example: filterWords = [ "arkham", "star wars", "lord of the rings" ];
+    var filterWords = []
+
+    // List of country codes (quoted, in lowercase) that are to be included
+    // Example: [ "fr", "de" ];
+    var countries = [ "gb" ];
+
+    // Start of actual code
+    var topdiv, i, s, spans, gparent;
+    var todelete = [];
+    var flag = "";
+    var itemName = "";
+
+    // Delete nodes and div before and after
+    function deleteLines(todelete, topdiv) {
+        var i;
+
+        for (i=0; i < todelete.length; i++ ) {
+            todelete[i].nextElementSibling.remove();
+            todelete[i].previousElementSibling.remove();
+            topdiv.removeChild(todelete[i]);
+        }
     }
 
-    var topdiv, i, spans, gparent;
-    var todelete = [];
 
     topdiv = document.getElementById("searchresultlist");
 
-    // Delete non-UK stores...
+    // Delete non requested stores...
 
-    // Find nodes to delete
+    // Find nodes to delete based on country
     spans = topdiv.getElementsByClassName("storename");
     for (i=0; i < spans.length ; i++) {
-        if (! spans[i].children[0].src.includes("/img/flags/GB.png")) {
+        // Grotty way to get the country code for the store
+        flag = spans[i].children[0].src.substr(-6, 2).toLowerCase();
+        if (countries.indexOf(flag) === -1) {
             gparent = spans[i].parentNode.parentNode;
             todelete.push(gparent);
         }
     }
+    deleteLines(todelete, topdiv);
 
-    // Delete nodes and div before and after
-    for (i=0; i < todelete.length; i++ ) {
-        todelete[i].nextElementSibling.remove();
-        todelete[i].previousElementSibling.remove();
-        topdiv.removeChild(todelete[i]);
+    // Find nodes to delete based on keyword filters
+
+    if (filterWords.length > 0) {
+        todelete = [];
+        spans = topdiv.getElementsByClassName("name");
+        for (i=0 ; i < spans.length ; i++) {
+            itemName = spans[i].children[0].textContent.toLowerCase();
+            for (s=0; s < filterWords.length; s++ ) {
+                if (itemName.includes(filterWords[s])) {
+                    gparent = spans[i].parentNode.parentNode;
+                    todelete.push(gparent);
+                    break;
+                }
+            }
+        }
+        deleteLines(todelete, topdiv);
     }
 
     // Sort by date
@@ -57,9 +87,9 @@
         if (aText < bText) return 1;
         return 0;
     })
-    for (i=0; i < spansArray.length; i++) {
-        console.log(spansArray[i].parentNode.getElementsByTagName("div")[0].textContent.valueOf());
-    }
+    //for (i=0; i < spansArray.length; i++) {
+        //console.log(spansArray[i].parentNode.getElementsByTagName("div")[0].textContent.valueOf());
+    //}
 
     var gparentPrev, gparentNext;
 
